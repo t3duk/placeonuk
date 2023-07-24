48a34aabda2e3aef77bf1f6cd4b67f34565ce5a2
Teddy Brine <ted@daad.wtf>
2023-07-24 22:41:14 +0100

    sdf

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
-var order = data.order;
-var time = data.time;
-var status;
-if (data.status == 0) {
-  status = "Pending";
-} else if (data.status == 1) {
-  status = "Executed";
-}
-var orderHtml = document.getElementById("order");
-var timeHtml = document.getElementById("time");
-var statusHtml = document.getElementById("status");
-orderHtml.innerHTML = order;
-timeHtml.innerHTML = time;
-statusHtml.innerHTML = status;
-
 setInterval(() => {
   fetch("https://api.daad.wtf/uop")
     .then((res) => res.json())
     .then((data) => {
-      var order = data.order;
-      var time = data.time;
       var status;
       if (data.status == 0) {
         status = "Pending";
       } else if (data.status == 1) {
         status = "Executed";
       }
-      var orderHtml = document.getElementById("order");
-      var timeHtml = document.getElementById("time");
-      var statusHtml = document.getElementById("status");
-      orderHtml.innerHTML = order;
-      timeHtml.innerHTML = time;
+      const orderHtml = document.getElementById("order");
+      const timeHtml = document.getElementById("time");
+      const statusHtml = document.getElementById("status");
+      orderHtml.innerHTML = data.order;
+      timeHtml.innerHTML = data.time;
       statusHtml.innerHTML = status;
     });
 }, 1000);
