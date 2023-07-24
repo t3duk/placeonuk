50fe4e969c05638232deaa92fe4d79df9fd9b059
Teddy Brine <ted@daad.wtf>
2023-07-24 22:38:06 +0100

    sdf

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ setInterval(() => {
       const order = data.order;
       const time = data.time;
       var status;
-      if (status == 0) {
+      if (data.status == 0) {
         status = "Pending";
-      } else if (status == 1) {
+      } else if (data.status == 1) {
         status = "Executed";
       }
       const orderHtml = document.getElementById("order");
